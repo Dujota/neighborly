0e3a6fe937a8d1718783cd8c68a1951dd35d9218
Denis <ddujota@internationalsafety.com>
2020-03-30 17:51:31 -0400

    setup: link with csrf token to pass protect from forgery - error logging - http link - create appollo client instance

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -1,5 +1,3 @@
-// app/javascript/utils/apollo.js
-
 // client
 import { ApolloClient } from 'apollo-client';
 // cache
@@ -16,3 +14,70 @@ export const createCache = () => {
   }
   return cache;
 };
+
+// getToken from meta tags
+const getToken = () => document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const token = getToken();
+const setTokenForOperation = async operation =>
+  operation.setContext({
+    headers: {
+      'X-CSRF-Token': token,
+    },
+  });
+// link with token
+const createLinkWithToken = () =>
+  new ApolloLink(
+    (operation, forward) =>
+      new Observable(observer => {
+        let handle;
+        Promise.resolve(operation)
+          .then(setTokenForOperation)
+          .then(() => {
+            handle = forward(operation).subscribe({
+              next: observer.next.bind(observer),
+              error: observer.error.bind(observer),
+              complete: observer.complete.bind(observer),
+            });
+          })
+          .catch(observer.error.bind(observer));
+        return () => {
+          if (handle) handle.unsubscribe();
+        };
+      })
+  );
+
+// log erors
+
+/**
+ * In production, it makes more sense to use an exception tracking service (e.g., Sentry or Honeybadger):
+ *
+ * just override the logError function to send errors to the external system
+ */
+const logError = error => console.error(error);
+// create error link
+const createErrorLink = () =>
+  onError(({ graphQLErrors, networkError, operation }) => {
+    if (graphQLErrors) {
+      logError('GraphQL - Error', {
+        errors: graphQLErrors,
+        operationName: operation.operationName,
+        variables: operation.variables,
+      });
+    }
+    if (networkError) {
+      logError('GraphQL - NetworkError', networkError);
+    }
+  });
+
+// http link
+const createHttpLink = () =>
+  new HttpLink({
+    uri: '/graphql',
+    credentials: 'include',
+  });
+
+export const createClient = (cache, requestLink) =>
+  new ApolloClient({
+    link: ApolloLink.from([createErrorLink(), createLinkWithToken(), createHttpLink()]),
+    cache,
+  });
