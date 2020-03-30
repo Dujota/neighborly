4575216883c39f115127082642586ae8774ce3eb
Denis <ddujota@internationalsafety.com>
2020-03-29 21:06:53 -0400

    added the listing type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :listings,
+          [Types::ListingType],
+          null: false,
+          description: "Returns all the listings in neighbourly"
+
+    def listings
+      Listing.All
     end
   end
 end
