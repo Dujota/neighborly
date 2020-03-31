1c1997c54da9af3ca8ec9046ed52a767fbf0ecd2
Denis <ddujota@internationalsafety.com>
2020-03-31 02:03:24 -0400

    setup the listings component with a gql-tag query requesting the list of seeded user and listing

---
+++ app/javascript/components/Listings/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ListingsQuery = gql`
+  {
+    listings {
+      id
+      title
+      description
+      imageUrl
+      createdAt
+      user {
+        id
+        email
+      }
+    }
+  }
+`;
+
+// eslint-disable-next-line react/display-name
+export default () => (
+  <Query query={ListingsQuery}>
+    {({ data, loading }) => (
+      <div>
+        {loading
+          ? 'loading...'
+          : data.listings.map(({ title, id, user }) => (
+              <div key={id}>
+                <b>{title}</b> {user ? `added by ${user.email}` : null}}
+              </div>
+            ))}
+      </div>
+    )}
+  </Query>
+);
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
 
-render(<Provider>👻</Provider>, document.querySelector('#root'));
+// Components
+import Listing from '../components/Listings';
+
+render(
+  <Provider>
+    <Listing />
+  </Provider>,
+  document.querySelector('#root')
+);
