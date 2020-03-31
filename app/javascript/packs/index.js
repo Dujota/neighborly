0974c8b2fe3e3846c98cab99a00a05f36b41c37f
Denis <ddujota@internationalsafety.com>
2020-03-31 01:41:16 -0400

    updarted reactdom render

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,5 @@
-console.log('👻')
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+
+render(<Provider>👻</Provider>, document.querySelector('#root'));
