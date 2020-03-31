780775ce2be2feaf58a5bb405809b9e95fa3124d
Denis <ddujota@internationalsafety.com>
2020-03-31 00:52:27 -0400

    added apollo provider component

---
+++ app/javascript/components/Provider/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { createCache, createClient } from '../../utils/apollo';
+
+export default ({ children }) => <ApolloProvider client={createClient(createCache())}>{children}</ApolloProvider>;
