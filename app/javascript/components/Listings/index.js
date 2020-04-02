2a089bd0f3066419068044da55299a08539e3752
David Bland <davidbland@hotmail.com>
2020-04-02 15:04:01 -0400

    added react-moment to format date and added the creation date to the listing items instead of dummy data

--- app/javascript/components/Listings/index.js
+++ app/javascript/components/Listings/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'react-moment';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -25,7 +26,7 @@ export default () => (
         <div id="listings">
           {loading
             ? 'loading...'
-            : data.listings.map(({ title, id, user }) => (
+            : data.listings.map(({ title, id, user, createdAt }) => (
                 <div className="listing-item" key={id}>
                   <div className="listing-image-section">
                     <img src="https://cdn2.iconfinder.com/data/icons/grocery-store-solid/64/Grocery_food-36-512.png" />
@@ -36,7 +37,9 @@ export default () => (
                     <p className="listing-details">details...</p>
                   </div>
                   <div className="listing-action-section">
-                    <span className="listing-date">April 1st 2020</span>
+                    <span className="listing-date">
+                      <Moment format="DD/MM/YYYY">{createdAt}</Moment>
+                    </span>
                     <a href={`/listingInfo/${id}`}><button className="listing-btn">More Details</button></a>
                   </div>
                 </div>
