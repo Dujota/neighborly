8406ef2c55a8bc51e98a9d4c76187eb4dce1d02e
Denis S Dujota <30379369+Dujota@users.noreply.github.com>
2020-04-01 21:57:37 -0400

    Create Listings mutation (#11)
    * bundle add rspec-rails and factory-bot-rails
    
    * expose factorybot methods to rspec tests
    
    * migrate to psql 12.2
    
    * added factories for user and listing and created graphql test file
    
    * added test for listing query and moved all dpreceaition warnings to a log file
    
    * added pry to dev and test and fixed expectation since schema.execute returns a json hash
    
    * eageerload the users on the listings query
    
    * Added graphql-tag loader to webpack build output
    
    * added user query and passed in the current user context to graphql queries
    
    * added base mutation inheritence
    
    * create listing mutation completed,
    - TODO: when signup-signin implemented can remove the fallback since current user context = nil atm

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,8 +1,15 @@
 module Mutations
-  class BaseMutation < GraphQL::Schema::RelayClassicMutation
-    argument_class Types::BaseArgument
-    field_class Types::BaseField
-    input_object_class Types::BaseInputObject
-    object_class Types::BaseObject
+
+  # BaseMutation < GraphQL::Schema::RelayClassicMutation  <-- default, replaced with simpler Simple mutation
+  class BaseMutation < GraphQL::Schema::Mutation
+
+    # ENABLE OPTIONS BELOW IF USING RelayClassicMutation
+
+    # argument_class Types::BaseArgument
+    # field_class Types::BaseField
+    # input_object_class Types::BaseInputObject
+    # object_class Types::BaseObject
+
+    # place all general mutation helpershere
   end
 end
