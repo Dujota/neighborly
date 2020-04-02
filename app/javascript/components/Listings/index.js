090bb444ea00bc3d8d455ce6f0a82b6b51e952e9
David Bland <davidbland@hotmail.com>
2020-04-02 13:56:28 -0400

    listing component styling first draft. Colour scheme adjustments expected

--- app/javascript/components/Listings/index.js
+++ app/javascript/components/Listings/index.js
@@ -21,29 +21,28 @@ const ListingsQuery = gql`
 // eslint-disable-next-line react/display-name
 export default () => (
   <Query query={ListingsQuery}>
-    <div id="listings">
       {({ data, loading }) => (
-        <div>
+        <div id="listings">
           {loading
             ? 'loading...'
             : data.listings.map(({ title, id, user }) => (
                 <div className="listing-item" key={id}>
                   <div className="listing-image-section">
-                    <img />
+                    <img src="https://cdn2.iconfinder.com/data/icons/grocery-store-solid/64/Grocery_food-36-512.png" />
                   </div>
                   <div className="listing-info-section">
                     <h3 className="listing-title">{title}</h3>
-                    <span className="listing-user">{user ? `added by ${user.email}` : null}</span>
+                    <span className="listing-user">added by <span>{user ? user.email : null}</span></span>
                     <p className="listing-details">details...</p>
                   </div>
                   <div className="listing-action-section">
-                    <span className="listing-date"></span>
+                    <span className="listing-date">April 1st 2020</span>
                     <button className="listing-btn">More Details</button>
                   </div>
                 </div>
               ))}
         </div>
       )}
-    </div>
+
   </Query>
 );
