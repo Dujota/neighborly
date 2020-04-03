c0fb621bf23a75f3a4b5eac98066df953396f1ab
Denis <ddujota@internationalsafety.com>
2020-04-03 14:14:10 -0400

    fixed the missing stylesheet definition in manifest, updated app.css.scss, created base mutation helpers for auth on the update and delete mutations, created the delete mutation and added to types. add error handling and auth handling onthe resolvers

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_user, mutation: Mutations::CreateUser
     field :create_listing, mutation: Mutations::CreateListing
     field :update_listing, mutation: Mutations::UpdateListing
+    field :delete_listing, mutation: Mutations::DeleteListing
   end
 end
