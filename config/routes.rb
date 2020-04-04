8773657eea976d33832ade2dd3cd0bdb6b7f87d8
Denis S Dujota <30379369+Dujota@users.noreply.github.com>
2020-04-04 12:55:17 -0400

    upated the routing names (#22)

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Disable devise routes until further notice
-  devise_for :users
+  devise_for :users, :path_names => {
+                       :sign_in => "signin",
+                       :sign_out => "signout",
+                       :sign_up => "signup",
+                     }
+
+  # SESSIONS AND REGISTRATIONS
+  get "/signin" => redirect("/users/signin")
+  get "/signup" => redirect("/users/signup")
+  get "/register" => redirect("/users/signup")
 
   root "listing#index"
 end
