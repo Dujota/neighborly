f2da75d7a0e52ed5a67dab4af0c848ae2f207056
Denis <ddujota@internationalsafety.com>
2020-04-03 22:26:07 -0400

    fixed listing to Listing

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,20 +4,26 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :listings,
+          # [Type] represents a list of Types
           [Types::ListingType],
           null: false,
           description: "Returns all the listings in neighbourly"
 
     def listings
+      # Not Ideal, but preload users with Listings to solve N+1, look at batch loading.
       Listing.includes(:user)
     end
 
-    field :listing, Types::ListingType, null: false, description: "Returns a listing in neighbourly based on an id" do
+    field :listing,
+          # Singular Type
+          Types::ListingType,
+          null: true,
+          description: "Returns a listing in neighbourly based on an id" do
       argument :id, ID, required: true
     end
 
     def listing(id:)
-      listing.find(id)
+      Listing.find id
     end
 
     field :me,
