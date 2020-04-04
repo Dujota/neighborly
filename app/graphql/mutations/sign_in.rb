30eebcdc197889de46132c5f8ba549daaf152a0d
Denis S Dujota <30379369+Dujota@users.noreply.github.com>
2020-04-04 01:44:17 -0400

    Implement sign in (#20)
    * udpated syntax on the test with dig of hash)
    
    * added the sign in mutation
    
    * fixe derrors

---
+++ app/graphql/mutations/sign_in.rb
@@ -0,0 +1,38 @@
+module Mutations
+  class SignIn < BaseMutation
+    description "Sign in User"
+
+    argument :credentials, Types::AuthProviderCredentialsInput, required: false
+
+    field :errors, [String], null: false
+    field :token, String, null: true
+    field :user, Types::UserType, null: true
+
+    def resolve(credentials: nil)
+      # Initial Basic Validation
+      return GraphQL::ExecutionError.new("Login Credentials Required") unless credentials
+
+      user = User.find_for_database_authentication(email: credentials[:email])
+
+      # If authentication fails
+      return GraphQL::ExecutionError.new("User with #{email} not registered on this application") unless user
+
+      # Check that the password for the fetched user, matches the one provided by mutation
+      if user.valid_password?(credentials[:password])
+        context[:current_user] = user
+
+        # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
+        crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+        token = crypt.encrypt_and_sign("user-id:#{user.id}")
+
+        {
+          user: user,
+          token: token,
+          errors: [],
+        }
+      else
+        { errors: user.errors.full_messages }
+      end
+    end
+  end
+end
