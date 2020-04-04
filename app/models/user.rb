2143b956ad22b8861bc046eac8ff47f54fe6696a
Denis <ddujota@internationalsafety.com>
2020-04-04 14:33:09 -0400

    added user authority methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,37 @@ class User < ApplicationRecord
 
   # Validations
   validates :email, presence: true, uniqueness: true
+
+  # USER AUTHORITY METHODS
+  def add_role(role)
+    self.roles << role
+  end
+
+  def add_role_by_role_name(role_name)
+    self.roles << Role.find_by_name(role_name)
+  end
+
+  def role_names
+    roles.map { |role| role.name }
+  end
+
+  def is_admin?
+    is_role_by_name?(:admin)
+  end
+
+  def is_role?(role)
+    self.roles.include?(role)
+  end
+
+  def is_role_by_name?(role_name)
+    self.roles.include?(Role.find_by_name(role_name))
+  end
+
+  # Profile Method
+  def find_or_build_profile
+    if profile.nil?
+      build_profile
+    end
+    profile
+  end
 end
