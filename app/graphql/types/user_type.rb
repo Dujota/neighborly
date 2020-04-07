f09ce57b0a6f322f544ba2157f33350145032452
Denis <ddujota@internationalsafety.com>
2020-04-07 02:03:10 -0400

    added the edit button and it is conditionally rendered

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,12 +2,13 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :email, String, null: false
+    field :is_admin, String, null: true
 
-    def full_name
+    def is_admin
       # `object` references the user instance
       #  for profile [object.first_name, object.last_name].compact.join(" ")
 
-      "check the profile of #{object.email}"
+      object.is_admin?
     end
   end
 end
--- app/javascript/components/Listings/listing_card.jsx
+++ app/javascript/components/Listings/listing_card.jsx
@@ -1,42 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// GraphQl
+import gql from 'graphql-tag';
+
 // Utilities
 import Moment from 'react-moment';
 
 // Components
+import { Query } from 'react-apollo';
 import Link from '../link';
 
+const CURRENT_USER = gql`
+  {
+    currentUser {
+      id
+      isAdmin
+    }
+  }
+`;
+
 const ListingCard = ({ id, createdAt, title, user, description }) => (
-  <div className="listing-item" key={id}>
-    <span className="listing-date">
-      <Moment format="DD/MM/YYYY">{createdAt}</Moment>
-    </span>
+  <Query query={CURRENT_USER}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
 
-    <div className="listing-image-section">
-      <img
-        src="https://cdn2.iconfinder.com/data/icons/grocery-store-solid/64/Grocery_food-36-512.png"
-        alt="listing-avatar"
-      />
-    </div>
+      return (
+        <div className="listing-item" key={id}>
+          <span className="listing-date">
+            <Moment format="DD/MM/YYYY">{createdAt}</Moment>
+          </span>
 
-    <div className="listing-info-section">
-      <h3 className="listing-title">{title}</h3>
+          <div className="listing-image-section">
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/grocery-store-solid/64/Grocery_food-36-512.png"
+              alt="listing-avatar"
+            />
+          </div>
 
-      {user && (
-        <span className="listing-user">
-          added by <span>{user.email}</span>
-        </span>
-      )}
+          <div className="listing-info-section">
+            <h3 className="listing-title">{title}</h3>
 
-      <p className="listing-details">{description}</p>
-    </div>
-    <div className="listing-action-section">
-      <Link path={`/listing?id=${id}`} className="btn listing-btn btn-secondary">
-        More Details
-      </Link>
-    </div>
-  </div>
+            {user && (
+              <span className="listing-user">
+                added by <span>{user.email}</span>
+              </span>
+            )}
+
+            <p className="listing-details">{description}</p>
+          </div>
+          <div className="listing-action-section">
+            <Link path={`/listing?id=${id}`} className="btn listing-btn btn-secondary">
+              More Details
+            </Link>
+            {console.log(data)}
+
+            {(data.currentUser.id === user.id || data.currentUser.isAdmin) && (
+              <Link path={`/listing?id=${id}&edit=true`} className="btn listing-btn btn-secondary">
+                Edit
+              </Link>
+            )}
+          </div>
+        </div>
+      );
+    }}
+  </Query>
 );
 
 ListingCard.propTypes = {
