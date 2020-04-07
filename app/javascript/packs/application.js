ba82c43a204095d554b117af4f456d20f95b631f
Denis S Dujota <30379369+Dujota@users.noreply.github.com>
2020-04-06 20:36:25 -0400

    Deploy heroku (#29)
    * asset compilation fix for heroku
    
    * update manifest

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 require('@rails/ujs').start();
 require('@rails/activestorage').start();
 require('channels');
-
 require('../common/flash');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
