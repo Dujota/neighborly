4847fd4ea96616de7a430c1250601a5c6849a890
Denis <ddujota@internationalsafety.com>
2020-04-07 00:45:07 -0400

    refactored the listing component

---
+++ app/javascript/components/Listings/listing_card.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Utilities
+import Moment from 'react-moment';
+
+// Components
+import Link from '../link';
+
+const ListingCard = ({ id, createdAt, title, user, description }) => (
+  <div className="listing-item" key={id}>
+    <span className="listing-date">
+      <Moment format="DD/MM/YYYY">{createdAt}</Moment>
+    </span>
+
+    <div className="listing-image-section">
+      <img
+        src="https://cdn2.iconfinder.com/data/icons/grocery-store-solid/64/Grocery_food-36-512.png"
+        alt="listing-avatar"
+      />
+    </div>
+
+    <div className="listing-info-section">
+      <h3 className="listing-title">{title}</h3>
+
+      {user && (
+        <span className="listing-user">
+          added by <span>{user.email}</span>
+        </span>
+      )}
+
+      <p className="listing-details">{description}</p>
+    </div>
+    <div className="listing-action-section">
+      <Link path={`/listing?id=${id}`} className="btn listing-btn btn-secondary">
+        More Details
+      </Link>
+    </div>
+  </div>
+);
+
+ListingCard.propTypes = {
+  id: PropTypes.string,
+  createdAt: PropTypes.string,
+  title: PropTypes.string,
+  user: PropTypes.object,
+  description: PropTypes.string,
+};
+
+export default ListingCard;
