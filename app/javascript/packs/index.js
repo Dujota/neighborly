747754c0a293eb2ee2de94ed8639fa0d1eae342a
Denis <ddujota@internationalsafety.com>
2020-04-07 15:41:30 -0400

    added layout compoennts to the landing page

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,9 +4,13 @@ import Provider from '../components/Provider';
 
 // Components
 import Listings from '../components/Listings';
+import Dashboard from '../components/Dashboard';
+import MapBox from '../components/Mapbox';
 
 render(
   <Provider>
+    <Dashboard />
+    <MapBox />
     <Listings />
   </Provider>,
   document.querySelector('#root')
