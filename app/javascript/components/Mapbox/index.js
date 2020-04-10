2dfced474c8dbf4c7eaea4f90a67601c1b88082c
David Bland <david.bland@hotmail.com>
2020-04-10 16:29:08 -0400

    Details render button (#37)
    * added a Home component and ListingContext
    
    * Now using context to link listings id state to mapbox
    
    * added preventDefault on button Listings button click
    
    * slight change to spacing on Link tag
    
    * converted functionality to useApolloClient
    
    * added default value for selectedListingId
    
    * removed listingsContext file
    
    Co-authored-by: David Bland <davidbland@hotmail.com>

--- app/javascript/components/Mapbox/index.js
+++ app/javascript/components/Mapbox/index.js
@@ -1,4 +1,28 @@
 /* eslint-disable react/display-name */
 import React from 'react';
+import { useQuery } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default () => <article id="mapbox-component">MAPBOX COMPONENT</article>;
+// Component
+import Listing from "../Listing"
+
+const GET_LISTING_ID = gql`
+    {
+        selectedListingId @client
+    } 
+`
+;
+
+export default () => {
+    
+    const { data, loading, error } = useQuery(GET_LISTING_ID);
+   
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
+    return (
+        <article id="mapbox-component">
+         { data && <Listing listingId={data.selectedListingId} /> }
+        </article>
+    );
+}
