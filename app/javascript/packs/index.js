3bad69b334bea7857d7d81881e008790bd15040d
Denis S Dujota <30379369+Dujota@users.noreply.github.com>
2020-04-10 21:44:23 -0400

    Layout containers (#43)
    * added preliminary card
    
    * added the preliminary positioning

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 // Components
-import Home from "../components/Home"
+import Home from '../components/Home';
 import Provider from '../components/Provider';
 
 render(
