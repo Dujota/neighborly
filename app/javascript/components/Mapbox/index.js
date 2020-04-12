4991b7c5475c77bfcff8a9ef3ec6341e02439c9c
Denis <ddujota@internationalsafety.com>
2020-04-12 12:40:00 -0400

    Added the form group styling standardization from material design

--- app/javascript/components/Mapbox/index.js
+++ app/javascript/components/Mapbox/index.js
@@ -25,6 +25,44 @@ export default () => {
   return (
     <article id="mapbox-component" className="card">
       MAPBOX
+      <div className="container">
+        <form>
+          <h1>Material Design formular</h1>
+
+          {/* <div className="form-group">
+            <select>
+              <option>Value 1</option>
+              <option>Value 2</option>
+            </select>
+            <label htmlFor="select" className="control-label">
+              Selectbox
+            </label>
+            <i className="bar"></i>
+          </div> */}
+
+          <div className="form-group">
+            <input type="text" required="required" />
+            <label htmlFor="input" className="control-label">
+              Textfield
+            </label>
+            <i className="bar"></i>
+          </div>
+
+          <div className="form-group">
+            <textarea required="required"></textarea>
+            <label htmlFor="textarea" className="control-label">
+              Textarea
+            </label>
+            <i className="bar"></i>
+          </div>
+        </form>
+
+        <div className="button-container">
+          <button type="button" className="button">
+            <span>Submit</span>
+          </button>
+        </div>
+      </div>
     </article>
   );
 };
