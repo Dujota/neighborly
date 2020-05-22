9fa02d4a7dc71d145fbfea81b34458cda94e7d3a
David Bland <davidbland@hotmail.com>
2020-05-22 17:46:41 -0400

    Created a before_create on the user to check the value of the location user input. Called locationIQ api depending on the input

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  require "net/http"
+  require "json"
+  require "openssl"
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  #  Callbacks
 
   #  Assosiactions
   has_one :profile, autosave: true
@@ -25,6 +28,44 @@ class User < ApplicationRecord
 
   #  Allows user to receive a hash with nested attributes to populate profile
   accepts_nested_attributes_for :profile
+
+  # Before Creating a User, run a method for locationIQ
+  before_create do
+    #Check the value of self.user_location
+    #If self.user_location starts with '{' then it is holding the users coords
+    #Might need to check if nil first?
+    if user_location.starts_with?("{")
+      get_location_name(user_location)
+    else
+      get_coords(user_location)
+    end
+  end
+
+  #API call to locationIQ
+  def get_coords(location_name)
+    #Call api with the location coords to return the coords / set the lat long values for locations
+    url = "https://us1.locationiq.com/v1/search.php?key=2081a07b6ccffd&q="+location_name+"&format=json"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    res = JSON.parse(response)
+    lat = res[0]["lat"]
+    long = res[0]["lon"]
+    display_name = res[0]["display_name"]
+    #Set locations lat long and display_name with the above values
+  end
+
+  def get_location_name(location_coords)
+    #Call api with the location name to return the name / set the name value for the locations
+    coords = JSON.parse(location_coords)
+    url = "https://us1.locationiq.com/v1/reverse.php?key=2081a07b6ccffd&lat="+coords["latitude"].to_s+"&lon="+coords["longitude"].to_s+"&format=json"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    res = JSON.parse(response)
+    lat = res["lat"]
+    long = res["lon"]
+    display_name = res["display_name"]
+    #Set locations lat long and display_name with the above values
+  end
 
   # USER AUTHORITY METHODS
   def add_role(role)
